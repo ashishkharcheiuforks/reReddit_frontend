d0cf579a1f51b2acd7724cc803646c90edded47e
Clint <cdunn6754@gmail.com>
2018-10-21 15:18:44 -0600

    Adds very basic step in getting data from api
    Performs an axios call from the User component to get
    at list of the usernames.

--- src/components/users/index.js
+++ src/components/users/index.js
@@ -1,17 +1,32 @@
 import React, { Component } from "react";
+import axios from 'axios';
  
 class Users extends Component {
+  constructor(props) {
+    super(props);
+    
+    this.state = {
+        users: []
+    }
+  }
+  
+  componentDidMount() {
+    axios.get('http://127.0.0.1:8000/users/')
+      .then(response => this.setState({users: response.data}))
+  }
+  
+  
+  
   render() {
+    const user_list = this.state.users.map(
+      (user, idx) => <li key={idx}>{user.username}</li>
+    )    
     return (
       <div>
         <h2>Users</h2>
         <p>List of Users:</p>
         <ol>
-          <li>Nulla pulvinar diam</li>
-          <li>Facilisis bibendum</li>
-          <li>Vestibulum vulputate</li>
-          <li>Eget erat</li>
-          <li>Id porttitor</li>
+          {user_list}
         </ol>
       </div>
     );

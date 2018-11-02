e6609a2023cbfe8163faab11a817c26c5058f68e
Clint <cdunn6754@gmail.com>
2018-11-02 15:23:42 -0600

    Incorporates dropdown for feed selection

--- src/components/NavBar/index.js
+++ src/components/NavBar/index.js
@@ -1,5 +1,10 @@
 import React from 'react';
-import { Navbar, NavItem, Nav } from 'react-bootstrap';
+import { Navbar,
+  NavItem,
+  Nav,
+  MenuItem,
+  NavDropdown,
+} from 'react-bootstrap';
 import { LinkContainer } from 'react-router-bootstrap';
 
 import "./styles.css"
@@ -7,13 +12,21 @@ import "./styles.css"
 const NavBar = () => (
   
   <div>
-    <Navbar inverse>
+    <Navbar inverse fixed fluid>
       <Navbar.Header>
         <Navbar.Brand>
-          <a href="/">React-Bootstrap</a>
+          <a href="/">reReddit</a>
         </Navbar.Brand>
       </Navbar.Header>
       <Nav>
+        <NavDropdown className='feed-dropdown' eventKey={3} title="feed" id="basic-nav-dropdown">
+          <MenuItem eventKey={3.1}>Action</MenuItem>
+          <MenuItem eventKey={3.2}>Another action</MenuItem>
+          <MenuItem eventKey={3.3}>Something else here</MenuItem>
+          <MenuItem divider />
+          <MenuItem eventKey={3.4}>Separated link</MenuItem>
+      </NavDropdown>
+  
         <LinkContainer exact to="/users">
           <NavItem eventKey={1} href="#">Users</NavItem>
         </LinkContainer>

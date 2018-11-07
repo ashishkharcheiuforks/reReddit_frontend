92d3ceead5af9ec71479efb8886b316c178d9c78
Clint <cdunn6754@gmail.com>
2018-11-07 16:40:41 -0700

    Adds login to auth use modal
    Currently it looks like we are recieving a request
    error which isn't great but indicates that
    the wiring of the app is working anyways. It looks
    like the credentials aren't being passed along appropriately.
    
    I will do some tests next to make sure I am using axios correctly.

--- src/components/UserAuthModal/index.js
+++ src/components/UserAuthModal/index.js
@@ -1,19 +1,27 @@
 import React from 'react';
 import { connect } from 'react-redux';
-import { Modal } from 'react-bootstrap';
+import { Modal, Button} from 'react-bootstrap';
 
-const UserAuthModal = ({show,onHide}) => (
-  <Modal 
-    show={show}
-    onHide={onHide}
-  >
-    <Modal.Header closeButton>
-      <Modal.Title>Modal Title</Modal.Title>
-    </Modal.Header>
-    <Modal.Body>
-      <h4>This is the Bod</h4>
-    </Modal.Body>
-  </Modal>
-);
+const UserAuthModal = ({show,onHide,userLogin}) => {
+  
+  const credentials = {username: "Bobby", password: "TheEnd6754"}
+  return (
+    <Modal 
+      show={show}
+      onHide={onHide}
+    >
+      <Modal.Header closeButton>
+        <Modal.Title>Modal Title</Modal.Title>
+      </Modal.Header>
+      <Modal.Body>
+        <h4>This is the Bod</h4>
+        <Button onClick={ () => userLogin(credentials)}>
+          Login
+        </Button>
+      </Modal.Body>
+      
+    </Modal>
+  );
+}
 
 export default UserAuthModal;

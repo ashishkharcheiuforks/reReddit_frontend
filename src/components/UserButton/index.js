c58ecb8c2994500479b3e51d3c65148de9b9ab00
Clint <cdunn6754@gmail.com>
2018-11-07 10:28:28 -0700

    Move to bootstrap for buttons

--- src/components/UserButton/index.js
+++ src/components/UserButton/index.js
@@ -1,13 +1,18 @@
 import React from 'react';
-import { Button } from 'semantic-ui-react';
+import { Button } from 'react-bootstrap';
+
+import './styles.css';
 
 
-const UserButton = (props) => {  
+const UserButton = ({content, onClick, inverted}) => {
+  const userButtonId = inverted ? "user-button-inverted" : "user-button-default";
   return (
-    <div>
-      <Button {...props}/>
-    </div>  
-  );
+      <div>
+        <Button onClick={onClick} id={userButtonId} className="user-button">
+          {content}
+        </Button>
+      </div>  
+    );
 };
 
 export default UserButton;

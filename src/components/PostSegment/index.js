c03d6a69e8b74609ff8f0ec4dcb76785f9d6099e
Clint <cdunn6754@gmail.com>
2018-11-15 12:23:09 -0700

    Styling post segment

--- src/components/PostSegment/index.js
+++ src/components/PostSegment/index.js
@@ -5,7 +5,20 @@ import './styles.css'
 
 const PostSegment = ({post}) => (
   <Panel id="post-segment-panel">
-    <Panel.Body>{post.body}</Panel.Body>
+    <div id="post-segment-voter">
+      
+    </div>
+    
+    <div id='post-segment-text-container'>
+      <div id="post-segment-title">
+        {post.body}
+      </div>
+      <div id='post-segment-info'>
+        <strong>{post.sub}</strong> - posted by: {post.poster}
+      </div>
+    </div>
+
+
   </Panel>
 );
 

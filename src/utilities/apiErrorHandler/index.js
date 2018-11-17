859454ea0091886fd5eaf4836d0c6701c201eb00
Clint <cdunn6754@gmail.com>
2018-11-17 11:36:58 -0700

    Adds api error handler

---
+++ src/utilities/apiErrorHandler/index.js
@@ -0,0 +1,21 @@
+const apiRequestErrorHandler = (e) => {
+  try {
+    if (!e.response) {
+      return "Network Error";
+    } else if (e.response.data) {
+      const erd = e.response.data;
+      
+      if (erd.detail){
+        return erd.detail;
+      }
+      console.log(erd)
+      return erd[0];
+    }
+  }
+  catch (error) {
+    console.log(error)
+    return "Something went wrong, please contact the site administration."
+  }
+}
+
+export default apiRequestErrorHandler;

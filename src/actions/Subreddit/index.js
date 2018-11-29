ab38c6d772609b0133136ddd722a5b985f815949
Clint <cdunn6754@gmail.com>
2018-11-29 11:55:30 -0700

    Adds api and action for getting sub detail

---
+++ src/actions/Subreddit/index.js
@@ -0,0 +1,20 @@
+import {
+  FETCH_SUB_DETAIL_REQUEST,
+  FETCH_SUB_DETAIL_SUCCESS,
+  FETCH_SUB_DETAIL_FAILURE,
+  API_SUB_DETAIL,
+} from '../actionTypes'
+
+import { getSubDetailApi } from '../../api/Subreddit';
+
+export const makeSubDetailRequest = (subredditTitle) => (
+  {
+    type: API_SUB_DETAIL,
+    types: {
+      request: FETCH_SUB_DETAIL_REQUEST,
+      success: FETCH_SUB_DETAIL_SUCCESS,
+      failure: FETCH_SUB_DETAIL_FAILURE,
+    }
+    callAPI: getSubDetailApi(subredditTitle),
+  }
+)

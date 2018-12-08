08887ebdc3ca6934ca0de50b57d9372f025c0bed
Clint <cdunn6754@gmail.com>
2018-12-08 14:19:50 -0700

    Removes outdated userlist

--- src/reducers/index.js
+++ src/reducers/index.js
@@ -1,13 +1,11 @@
 import { combineReducers } from 'redux';
 
-import userList from './userList';
 import subreddit from './subreddit';
 import userAuth from './userAuth';
 import userAuthModal from './userAuthModal';
 import post from './post';
 
 const rootReducer = combineReducers({
-  userList,
   subreddit,
   userAuth,
   userAuthModal,

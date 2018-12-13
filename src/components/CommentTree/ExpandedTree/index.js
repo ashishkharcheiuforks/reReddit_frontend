5e2d1bfa4b544c2ac1dc60260ab5e4052e5c8481
Clint <cdunn6754@gmail.com>
2018-12-13 10:52:53 -0700

    Adds editor toggling to individul comments

--- src/components/CommentTree/ExpandedTree/index.js
+++ src/components/CommentTree/ExpandedTree/index.js
@@ -1,57 +1,91 @@
-import React from 'react';
+import React, { Component } from 'react';
 import { Button } from 'react-bootstrap';
 import { FaComment, FaArrowUp, FaArrowDown, } from 'react-icons/fa';
 
 import './styles.css';
+import CommentEditor from '../../CommentEditor';
 import CommentInfoLine from '../CommentInfoLine';
+import { withMaybe } from '../../../utilities/HOC';
 
-const ExpandedTree = (props) => {
+class ExpandedTree extends Component {
+  constructor(props) {
+    super(props);
+    
+    this.state={
+      showEditor: false,
+    }
+    
+    this.handleToggleEditor = this.handleToggleEditor.bind(this);
+  }
   
-  const {
-    childrenList,
-    body,
-    posterUsername,
-    upvotes,
-    created,
-    handleToggleCollapse:handleCollapse,
-  } = props;
+  handleToggleEditor() {
+    this.setState({
+      showEditor: !this.state.showEditor,
+    });
+  }
   
-  return (
-    <div className="comment-tree-content">
-      <div className="comment-voter-collapser">
-        <div className='comment-voter upvoter'>
-          <span onClick={() => false}>
-            <FaArrowUp />
-          </span>
-        </div>
-        <div className='comment-voter downvoter'>
-          <span onClick={() => false}>
-            <FaArrowDown />
-          </span>
-        </div>
-        <span onClick={() => handleCollapse()} className='thread-line-container'>
-          <div className='thread-line'/>
-        </span>
-      </div>
-      <div className="comment-panel">
-        <CommentInfoLine {...{posterUsername, upvotes, created}}/>
-        <div
-          className="comment-body-container"
-          dangerouslySetInnerHTML={{__html: body}}
-          />
-        <div className="comment-links">
-          <div className="comment-icon">
-            <FaComment/>
+  render() {
+    
+    const {
+      childrenList,
+      body,
+      posterUsername,
+      upvotes,
+      created,
+      handleToggleCollapse:handleCollapse,
+    } = this.props;
+    
+    const HideableEditor = withMaybe(
+      (props) => props.showEditor
+    )(CommentEditor);
+  
+    return (
+      <div className="comment-tree-content">
+        <div className="comment-voter-collapser">
+          <div className='comment-voter upvoter'>
+            <span onClick={() => false}>
+              <FaArrowUp />
+            </span>
           </div>
-          <Button bsSize='xsmall' className='comment-buttons'> Reply </Button>
-          <Button bsSize='xsmall' className='comment-buttons'> Share </Button>
+          <div className='comment-voter downvoter'>
+            <span onClick={() => false}>
+              <FaArrowDown />
+            </span>
+          </div>
+          <span onClick={() => handleCollapse()} className='thread-line-container'>
+            <div className='thread-line'/>
+          </span>
         </div>
-        <div className="children-container">
-          {childrenList}
+        <div className="comment-panel">
+          <CommentInfoLine {...{posterUsername, upvotes, created}}/>
+          <div
+            className="comment-body-container"
+            dangerouslySetInnerHTML={{__html: body}}
+            />
+          <div className="comment-links">
+            <div className="comment-icon">
+              <FaComment/>
+            </div>
+            <Button
+              bsSize='xsmall'
+              className='comment-buttons'
+              onClick={() => this.handleToggleEditor()}>
+                Reply
+            </Button>
+            <Button bsSize='xsmall' className='comment-buttons'> Share </Button>
+          </div>
+          <HideableEditor
+            postPk={2}
+            handleCreateComment={() => false}
+            showEditor={this.state.showEditor}
+          />
+          <div className="children-container">
+            {childrenList}
+          </div>
         </div>
       </div>
-    </div>
-  )
+    )
+  }
 }
 
 export default ExpandedTree;

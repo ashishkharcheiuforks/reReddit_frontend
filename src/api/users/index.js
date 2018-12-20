be57f809e998d590727470c5b4baea6c91cb3ff8
Clint <cdunn6754@gmail.com>
2018-12-20 09:20:22 -0700

    Cleanup by removing unecessary userlist api

--- src/api/users/index.js
+++
@@ -1,8 +0,0 @@
-import axios from 'axios'
-
-import { USER_LIST_URL} from '../constants'
-
-export const getUserListApi = () => {
-  return axios.get(USER_LIST_URL)
-          .then(response => response.data)
-}

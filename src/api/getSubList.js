f83ca5138806bc203c7b228358d26f651083e157
Clint <cdunn6754@gmail.com>
2018-12-20 14:59:08 -0700

    Removes old action file

--- src/api/getSubList.js
+++
@@ -1,8 +0,0 @@
-import axios from 'axios';
-
-import { SUB_LIST_URL } from './constants';
-
-export default const getSubListApi = () => {
-  return axios.get(SUB_LIST_URL)
-          .then(response => response.data)
-}

1bd0ec610dab3f5cf4b22a5eaef834014898bd2e
Clint <cdunn6754@gmail.com>
2018-12-26 12:31:16 -0700

    Adds dropdown styling for delete comment

--- src/components/EllipsisButton/index.js
+++ src/components/EllipsisButton/index.js
@@ -1,11 +1,23 @@
 import React from 'react';
 import { FaEllipsisH } from 'react-icons/fa';
-import { Button } from 'react-bootstrap';
+import { Dropdown, MenuItem } from 'react-bootstrap';
 
-const EllipsisButton = (props) => (
-  <Button bsSize='xsmall' className='comment-buttons'>
-    <FaEllipsisH />
-  </Button>
+import './styles.css';
+
+const EllipsisButton = ({dropDownOptions}) => (
+  
+  <Dropdown id="ellipsis-dropdown" >
+    <Dropdown.Toggle noCaret className="toggle-button">
+      <FaEllipsisH/>
+    </Dropdown.Toggle>
+    <Dropdown.Menu className="dropdown-menu">
+      <MenuItem eventKey="1">Delete</MenuItem>
+    </Dropdown.Menu>
+  </Dropdown>
 )
 
 export default EllipsisButton;
+
+  // <Button bsSize='xsmall' className='comment-buttons'>
+  //   <FaEllipsisH />
+  // </Button>

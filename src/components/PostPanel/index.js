22ae9f839883452a3430765c35daf30ed6c8a913
Clint <cdunn6754@gmail.com>
2019-01-01 15:40:52 -0700

    Add post panel click functionality

--- src/components/PostPanel/index.js
+++ src/components/PostPanel/index.js
@@ -1,5 +1,6 @@
-import React from 'react';
-import PropTypes from 'prop-types'
+import React, { Component } from 'react';
+import PropTypes from 'prop-types';
+import { withRouter } from 'react-router';
 import { Panel, Button, MenuItem } from 'react-bootstrap';
 import { FaShare } from 'react-icons/fa';
 import { Link } from 'react-router-dom';
@@ -9,75 +10,95 @@ import EllipsisButton from '../EllipsisButton';
 import { withMaybe } from '../../utilities/HOC';
 import './styles.css';
 
-
-const PostPanel = (props) => {
+class PostPanel extends Component {
   
-  const {
-    upvotes,
-    pk,
-    title,
-    subredditTitle,
-    posterUsername,
-    authUsername,
-    created,
-    voteDisplayState,
-    handleDeletePost,
-  } = props;
+  handlePanelClick = (e) => {
+    const {
+      subredditTitle,
+      pk,
+      history,
+    } = this.props;
+    
+    if (e.target instanceof HTMLDivElement) {
+      history.push(`/r/${subredditTitle}/postDetail/${pk}`)
+    }
+  };
   
-  const AuthEllipsis = withMaybe(
-    (props) => props.showEllipsis
-  )(EllipsisButton);
+  render() {
+    const {
+      upvotes,
+      pk,
+      title,
+      subredditTitle,
+      posterUsername,
+      authUsername,
+      created,
+      voteDisplayState,
+      handleDeletePost,
+      history,
+    } = this.props;
+    
+    const AuthEllipsis = withMaybe(
+      (props) => props.showEllipsis
+    )(EllipsisButton);
 
-  return (
-    <div className="post-segment-panel" >
-        <VoterContainer
-          upvotes={upvotes}
-          voteDisplayState={voteDisplayState || 0}
-          itemType={'post'}
-          itemPk={pk}
-        />
+  //
+  //<{`/r/${subredditTitle}`}>
+    return (
+      <div
+        className="post-segment-panel"
+        name="postPanel"
+        onClick={this.handlePanelClick}
+      >
+          <VoterContainer
+            upvotes={upvotes}
+            voteDisplayState={voteDisplayState || 0}
+            itemType={'post'}
+            itemPk={pk}
+          />
 
-      
-      <div className='post-segment-text-container'>
-        <div className="post-segment-title">
-          <Link to={"/r/" + subredditTitle + "/postDetail/" + pk}>
-            {title}
-          </Link>
-        </div>
         
-        <div className='post-segment-info'>
-          <strong>
-            <Link to={"/r/" + subredditTitle}>
-              r/{subredditTitle}
-            </Link>
-          </strong>
-          - posted by:
-          <a href="/#"> u/{posterUsername} </a>
-          {created}
-        </div>
-        
-        <div className='post-segment-links'>
-          <Button bsSize='xsmall' className='post-buttons'> X Comments </Button>
-          <Button bsSize='xsmall' className='post-buttons'>
-             <FaShare /> Share
-           </Button>
-          <Button bsSize='xsmall' className='post-buttons'> Save </Button>
-          <AuthEllipsis
-            showEllipsis={authUsername === posterUsername}
-          >
-            <MenuItem
-              eventKey={1}
-              onSelect={handleDeletePost}
+        <div className='post-segment-text-container'>
+          <div className="post-segment-title">
+            <Link
+              id="post-title"
+              to={"/r/" + subredditTitle + "/postDetail/" + pk}
             >
-              delete
-            </MenuItem>
-          </AuthEllipsis>
+              {title}
+            </Link>
+          </div>
+          
+          <div className='post-segment-info'>
+            <strong>
+              <Link to={`/r/${subredditTitle}`}>
+                r/{subredditTitle}
+              </Link>
+            </strong>
+            - posted by:
+            <a href="#"> u/{posterUsername} </a>
+            {created}
+          </div>
+          
+          <div className='post-segment-links'>
+            <Button bsSize='xsmall' className='post-buttons'> X Comments </Button>
+            <Button bsSize='xsmall' className='post-buttons'>
+               <FaShare /> Share
+             </Button>
+            <AuthEllipsis
+              showEllipsis={authUsername === posterUsername}
+            >
+              <MenuItem
+                eventKey={1}
+                onSelect={handleDeletePost}
+              >
+                delete
+              </MenuItem>
+            </AuthEllipsis>
+          </div>
         </div>
       </div>
-
-
-    </div>
-  );
+    );
+  }
 }
 
 PostPanel.propTypes = {
@@ -92,4 +113,4 @@ PostPanel.propTypes = {
   handleDeletePost: PropTypes.func,
 }
 
-export default PostPanel;
+export default withRouter(PostPanel);

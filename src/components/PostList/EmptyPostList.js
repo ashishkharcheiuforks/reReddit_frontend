5bf50c94372d91dc836f0a7e201080003f382a1a
Clint <cdunn6754@gmail.com>
2019-01-09 11:11:04 -0700

    Update empty post list message

--- src/components/PostList/EmptyPostList.js
+++ src/components/PostList/EmptyPostList.js
@@ -1,9 +1,9 @@
-import React from 'react';
+import React from "react";
 
 const EmptyPostList = () => (
-  <div className='empty-list-content'>
-    <h2>No posts have been made in this subreddit yet!</h2>
+  <div className="empty-list-content">
+    <h2>Such Empty!</h2>
   </div>
-)
+);
 
 export default EmptyPostList;

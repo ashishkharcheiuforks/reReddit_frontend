21d71bd64e9fce0f7a411e42302e3657010366f2
Clint <cdunn6754@gmail.com>
2019-01-15 10:36:25 -0700

    Changes new to all in navbar dropdown for subreddit list
    I was mistaken originally. 'new' is a sort selection, not a pseudo
    subreddit like `all`.

--- src/components/NavBar/NavBarDropdown.js
+++ src/components/NavBar/NavBarDropdown.js
@@ -1,48 +1,36 @@
-import React from 'react';
-import {
-  NavDropdown,
-  MenuItem
-} from 'react-bootstrap';
-import { LinkContainer } from 'react-router-bootstrap';
+import React from "react";
+import { NavDropdown, MenuItem } from "react-bootstrap";
+import { LinkContainer } from "react-router-bootstrap";
 
-import './styles.css'
+import "./styles.css";
 
-const NavBarDropdown = ({subscribed}) => {
-  
-  const subscribedSubs = subscribed.map( sub =>
+const NavBarDropdown = ({ subscribed }) => {
+  const subscribedSubs = subscribed.map(sub => (
     <LinkContainer key={sub.pk} exact to={"/r/" + sub.title}>
-      <MenuItem eventKey={sub.pk}>
-        {sub.title}
-      </MenuItem>
+      <MenuItem eventKey={sub.pk}>{sub.title}</MenuItem>
     </LinkContainer>
-  )
-  
+  ));
+
   return (
     <NavDropdown
-      className='feed-dropdown'
+      className="feed-dropdown"
       eventKey={3}
-      title='feed'
-      id='basic-nav-dropdown'
+      title="feed"
+      id="basic-nav-dropdown"
     >
-      <LinkContainer exact to='/'>
-        <MenuItem eventKey={3.1}>
-          Home
-        </MenuItem>
+      <LinkContainer exact to="/">
+        <MenuItem eventKey={3.1}>Home</MenuItem>
       </LinkContainer>
-      <LinkContainer exact to='/r/popular'>
-        <MenuItem eventKey={3.2}>
-          Popular
-        </MenuItem>
+      <LinkContainer exact to="/r/popular">
+        <MenuItem eventKey={3.2}>Popular</MenuItem>
       </LinkContainer>
-      <LinkContainer exact to='/r/new'>
-        <MenuItem eventKey={3.3}>
-          New
-        </MenuItem>
+      <LinkContainer exact to="/r/all">
+        <MenuItem eventKey={3.3}>All</MenuItem>
       </LinkContainer>
       <MenuItem divider eventKey={3.4} />
       {subscribedSubs}
     </NavDropdown>
-  )
-}
+  );
+};
 
 export default NavBarDropdown;

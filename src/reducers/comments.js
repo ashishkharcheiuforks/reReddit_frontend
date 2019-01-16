a4728db163736df7b8924b273334f4904f7b57f4
Clint <cdunn6754@gmail.com>
2019-01-16 15:13:07 -0700

    Adds a hacky solution to the nascent comment parent problem
    It is an issue on Github but basically if you comment on a
    comment that was itself just created (and not yet refectched
    from the backend) you will run into an error because the returned
    data from the create is used to populate the redux store
    and is inconsistent (does not have the children array) with
    the backend serializer used for fetching lists of comments
    that correspond to a post (that is the normal way of getting
    comments.

--- src/reducers/comments.js
+++ src/reducers/comments.js
@@ -11,84 +11,77 @@ import {
   UPDATE_COMMENT_REQUEST,
   UPDATE_COMMENT_SUCCESS,
   UPDATE_COMMENT_FAILURE,
-  COMMENT_VOTE_SUCCESS,
-} from '../actions/actionTypes';
-import {
-  updateObjectOnVote,
-  updateObject,
-} from '../utilities/reducerUtils';
-
+  COMMENT_VOTE_SUCCESS
+} from "../actions/actionTypes";
+import { updateObjectOnVote, updateObject } from "../utilities/reducerUtils";
 
 // Returns an object with rootCommentIds property to
 // That includes the newcomment id at the top
 const addRootCommentId = (state, newComment) => {
   let newRoots = state.rootCommentIds.slice();
-  newRoots.splice(0,0,newComment.pk);
+  newRoots.splice(0, 0, newComment.pk);
   return { rootCommentIds: newRoots };
-}
+};
 
 // Returns a new commentsById object with the parent object
 // updated to include the new child
 const addChildCommentId = (state, newComment) => {
   const parentId = newComment.parent;
   const oldParent = state.commentsById[parentId];
+  // This is a hack fix because the comment creation backend doesn't
+  // add a children array like it should. So if you comment on a nascent
+  // comment you will get an error because there is no children array
+  oldParent.children = oldParent.children || [];
   const newChildren = oldParent.children.slice();
-  newChildren.splice(0,0,newComment.pk);
+  newChildren.splice(0, 0, newComment.pk);
   const newParent = {
     ...oldParent,
-    children: newChildren,
+    children: newChildren
   };
-  return { ...state.commentsById, [parentId]: newParent, };
-}
+  return { ...state.commentsById, [parentId]: newParent };
+};
 
 // Handles adding the newComment id to either the rootCommentIds or
 // the children array of the parent (depending on which is appropriate)
 // Also adds the new comment to the commentsById object.
 const addComment = (state, newComment) => {
-  
   // First to some fixes particular to a nacsent comment
-  const {
-    vote_state,
-    ...rest
-  } = newComment;
+  const { vote_state, ...rest } = newComment;
   const updatedComment = {
     ...rest,
     voteDisplayState: vote_state,
-    created: 'just now'};
-    
+    created: "just now"
+  };
+
   // if it's a root comment
   if (newComment.post) {
-    return (
-      {
-        ...addRootCommentId(state, updatedComment),
-        commentsById: { ...state.commentsById, [newComment.pk]: updatedComment }
-      }
-    )
+    return {
+      ...addRootCommentId(state, updatedComment),
+      commentsById: { ...state.commentsById, [newComment.pk]: updatedComment }
+    };
   } else if (newComment.parent) {
     // or it it is a child comment
     const newCommentsById = addChildCommentId(state, updatedComment);
-    return (
-      {
-        commentsById: {
-          ...newCommentsById,
-          [newComment.pk]: updatedComment
-        }
+    return {
+      commentsById: {
+        ...newCommentsById,
+        [newComment.pk]: updatedComment
       }
-    )
+    };
   }
-}
+};
 
 // Update a particular comment with id and new object properties newProps
 const updateComment = (state, id, newProps) => {
   const oldComment = state.commentsById[id];
-  return ({
+  return {
     ...state,
     commentsById: {
       ...state.commentsById,
-      [id]: updateObject(oldComment, newProps),
-    },
-  });
-}
+      [id]: updateObject(oldComment, newProps)
+    }
+  };
+};
 
 const initialState = {
   commentsById: {},
@@ -97,16 +90,16 @@ const initialState = {
   error: null,
   loading: false,
   createCommentError: null,
-  createCommentLoading: false,
-}
+  createCommentLoading: false
+};
 
-const comments = (state=initialState, action) => {
+const comments = (state = initialState, action) => {
   switch (action.type) {
     case FETCH_POST_COMMENT_TREES_REQUEST:
       return {
         ...state,
-        loading: true,
-      }
+        loading: true
+      };
     case FETCH_POST_COMMENT_TREES_SUCCESS:
       return {
         ...state,
@@ -115,32 +108,32 @@ const comments = (state=initialState, action) => {
         rootCommentIds: action.data.result,
         error: null,
         loading: false,
-        createCommentError: null,
-      }
+        createCommentError: null
+      };
     case FETCH_POST_COMMENT_TREES_FAILURE:
       return {
         ...state,
-        error: action.error,
-      }
+        error: action.error
+      };
     case CREATE_COMMENT_SUCCESS:
       return {
         ...state,
         ...addComment(state, action.data),
         createCommentLoading: false,
-        createCommentError: null,
-      }
+        createCommentError: null
+      };
     case CREATE_COMMENT_REQUEST:
       return {
         ...state,
         createCommentLoading: true,
-        createCommentError: null,
-      }
+        createCommentError: null
+      };
     case CREATE_COMMENT_FAILURE:
       return {
         ...state,
         createCommentLoading: false,
-        createCommentError: action.error,
-      }
+        createCommentError: action.error
+      };
     case COMMENT_VOTE_SUCCESS:
       const commentId = action.data.comment;
       return {
@@ -149,35 +142,26 @@ const comments = (state=initialState, action) => {
           ...state.commentsById,
           [commentId]: updateObjectOnVote(
             state.commentsById[commentId],
-            action.data.vote_type,
-          ),
-        },
+            action.data.vote_type
+          )
+        }
       };
     case DELETE_COMMENT_SUCCESS:
-      return updateComment(
-        state,
-        action.data.pk,
-        {
-            deleted: true,
-        }
-      );
+      return updateComment(state, action.data.pk, {
+        deleted: true
+      });
     case UPDATE_COMMENT_SUCCESS:
-      return updateComment(
-        state,
-        action.data.pk,
-        {
-          ...action.data,
-        }
-      );
+      return updateComment(state, action.data.pk, {
+        ...action.data
+      });
     default:
-      return state
+      return state;
   }
-}
-
+};
 
 // // Selectors
 // Ordered array of root comments, based on api ordering
-export const getRootCommentPks = (state) => state.comments.rootCommentIds;
+export const getRootCommentPks = state => state.comments.rootCommentIds;
 
 export const getCommentById = (state, pk) => state.comments.commentsById[pk];
 
@@ -186,10 +170,9 @@ export const getPosterByCommentId = (state, pk) => {
   return state.comments.postersById[posterId];
 };
 
-export const getVoteDisplayStateById = (state, pk) => (
-  state.comments.commentsById[pk].voteDisplayState
-);
+export const getVoteDisplayStateById = (state, pk) =>
+  state.comments.commentsById[pk].voteDisplayState;
 
-export const getCommentsLoading = (state) => state.comments.loading;
+export const getCommentsLoading = state => state.comments.loading;
 
 export default comments;

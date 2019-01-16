b861ea2c5ac57b3ecb08c986e1da7abb5845f8e8
Clint <cdunn6754@gmail.com>
2019-01-16 12:18:02 -0700

    Updates some comment panel styling

--- src/components/Comment/CommentBody/index.js
+++ src/components/Comment/CommentBody/index.js
@@ -1,15 +1,14 @@
-import React, { Component, Fragment } from 'react';
-import { FaComment} from 'react-icons/fa';
-import { Button, MenuItem} from 'react-bootstrap';
-import PropTypes from 'prop-types';
+import React, { Component, Fragment } from "react";
+import { FaComment } from "react-icons/fa";
+import { Button, MenuItem } from "react-bootstrap";
+import PropTypes from "prop-types";
 
-import EllipsisButton from '../../EllipsisButton';
-import { withMaybe } from '../../../utilities/HOC';
+import EllipsisButton from "../../EllipsisButton";
+import { withMaybe } from "../../../utilities/HOC";
+import "./styles.css";
 
 class CommentBody extends Component {
-  
   render() {
-    
     const {
       body,
       posterUsername,
@@ -17,55 +16,51 @@ class CommentBody extends Component {
       pk,
       handleDeleteComment,
       handleToggleReplyEditor,
-      handleToggleUpdateEditor,
+      handleToggleUpdateEditor
     } = this.props;
-    
+
     const AuthEllipsis = withMaybe(
-      (props) => props.authUsername === props.posterUsername
+      props => props.authUsername === props.posterUsername
     )(EllipsisButton);
-    
+
     return (
       <Fragment>
         <div
           className="comment-body-container"
-          dangerouslySetInnerHTML={{__html: body}}
-          />
+          dangerouslySetInnerHTML={{ __html: body }}
+        />
         <div className="comment-links">
           <div className="comment-icon">
-            <FaComment/>
+            <FaComment />
           </div>
           <Button
-            bsSize='xsmall'
-            className='comment-buttons'
+            bsSize="xsmall"
+            className="comment-buttons"
             onClick={handleToggleReplyEditor}
           >
             Reply
           </Button>
-          <Button bsSize='xsmall' className='comment-buttons'>
-            Share
-          </Button>
+          {/* This may be nice but for now we don't have a comment detail page
+            <Button bsSize='xsmall' className='comment-buttons'>
+              Share
+            </Button>
+          */}
           <AuthEllipsis
             {...{
               authUsername,
-              posterUsername,
+              posterUsername
             }}
           >
-            <MenuItem
-              eventKey={2}
-              onSelect={handleToggleUpdateEditor}
-            >
+            <MenuItem eventKey={2} onSelect={handleToggleUpdateEditor}>
               edit
             </MenuItem>
-            <MenuItem
-              eventKey={1}
-              onSelect={handleDeleteComment}
-            >
+            <MenuItem eventKey={1} onSelect={handleDeleteComment}>
               delete
             </MenuItem>
           </AuthEllipsis>
         </div>
       </Fragment>
-    )
+    );
   }
 }
 
@@ -76,7 +71,7 @@ CommentBody.propTypes = {
   pk: PropTypes.number,
   handleDeleteComment: PropTypes.func,
   handleToggleReplyEditor: PropTypes.func,
-  handleToggleUpdateEditor: PropTypes.func,
-}
+  handleToggleUpdateEditor: PropTypes.func
+};
 
 export default CommentBody;

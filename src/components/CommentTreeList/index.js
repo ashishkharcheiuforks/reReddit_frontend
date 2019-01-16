239d69a6409c9a0446b82a2e9f2d3c12205fa984
Clint <cdunn6754@gmail.com>
2019-01-16 12:39:24 -0700

    Simplifies the comment editor container logic
    I had previously concocted this mess using mergeprops so that
    when used to create a new comment on a post the CommentEditorContainer
    could operate in isolation and grab the postPk using mapStateToProps.
    
    I have since decided that it's pretty much always a better bet to just
    pass the postPk down through a few components. When I looked into it
    the postPk was already available in the CommentTreeList which
    calls CommentEditorContainer, so it was easier than I thought.

--- src/components/CommentTreeList/index.js
+++ src/components/CommentTreeList/index.js
@@ -1,68 +1,65 @@
-import React from 'react';
-import PropTypes from 'prop-types';
+import React from "react";
+import PropTypes from "prop-types";
 
-import { PanelListLoader } from '../Loaders';
-import { ErrorAlert } from '../AlertMessage';
-import CommentContainer from '../../containers/CommentContainer';
-import CommentEditorContainer from '../../containers/CommentEditorContainer';
-import { withMaybe, withEither } from '../../utilities/HOC';
+import { PanelListLoader } from "../Loaders";
+import { ErrorAlert } from "../AlertMessage";
+import CommentContainer from "../../containers/CommentContainer";
+import CommentEditorContainer from "../../containers/CommentEditorContainer";
+import { withMaybe, withEither } from "../../utilities/HOC";
 
-const CommentTreeList = (props) => {
+const CommentTreeList = props => {
   const {
     rootCommentPks,
     error,
     loading,
+    postPk,
     createCommentError,
-    createCommentLoading,
+    createCommentLoading
   } = props;
-  
+
   if (error) {
-    return (
-      <ErrorAlert>
-        {error}
-      </ErrorAlert>
-    )
+    return <ErrorAlert>{error}</ErrorAlert>;
   }
-  
+
   let commentRootList = [];
   if (Array.isArray(rootCommentPks) && rootCommentPks.length) {
-    commentRootList =
-      rootCommentPks.map(rootPk => (
-        <CommentContainer
-          pk={rootPk}
-          key={rootPk}
-        />
-      ))
+    commentRootList = rootCommentPks.map(rootPk => (
+      <CommentContainer pk={rootPk} key={rootPk} />
+    ));
   }
 
   // Error with root comment creation
-  const AlertOnError = withMaybe(
-    (props) => props.children)(ErrorAlert)
-    
-  const CommentListWithLoading = withEither (
-    (props) => props.loading, PanelListLoader
-  )(() => commentRootList)
-  
-  return (
-        <div className='comment-tree-list-container'>
-          <AlertOnError children={createCommentError} />
-          <div className='top-comment-editor'>
-            <CommentEditorContainer rootComment={true} usage='create'/>
-          </div>
+  const AlertOnError = withMaybe(props => props.children)(ErrorAlert);
 
-          <ul>
-            <CommentListWithLoading loading={loading} panelNumber={10} />
-          </ul>
-        </div>
-  )
-}
+  const CommentListWithLoading = withEither(
+    props => props.loading,
+    PanelListLoader
+  )(() => commentRootList);
+
+  return (
+    <div className="comment-tree-list-container">
+      <AlertOnError children={createCommentError} />
+      <div className="top-comment-editor">
+        <CommentEditorContainer
+          postPk={postPk}
+          rootComment={true}
+          usage="create"
+        />
+      </div>
+
+      <ul>
+        <CommentListWithLoading loading={loading} panelNumber={10} />
+      </ul>
+    </div>
+  );
+};
 
 CommentTreeList.propTypes = {
   rootCommentPks: PropTypes.arrayOf(PropTypes.number),
   error: PropTypes.string,
   loading: PropTypes.bool,
   createCommentError: PropTypes.string,
-  createCommentLoading: PropTypes.bool,
-}
+  createCommentLoading: PropTypes.bool
+};
 
 export default CommentTreeList;
--- src/containers/CommentEditorContainer/index.js
+++ src/containers/CommentEditorContainer/index.js
@@ -1,86 +1,88 @@
-import React from 'react';
-import { connect } from 'react-redux';
-import PropTypes from 'prop-types';
+import React from "react";
+import { connect } from "react-redux";
+import PropTypes from "prop-types";
 
-import TextEditor from '../../components/TextEditor';
+import TextEditor from "../../components/TextEditor";
 import {
   makeCreateCommentRequest,
   makeUpdateCommentRequest
-} from '../../actions/Comments';
-import { getPostDetailPk } from '../../reducers/postDetail';
+} from "../../actions/Comments";
+import { getPostDetailPk } from "../../reducers/postDetail";
 
-const CommentEditorContainer = (props) => (
-    <TextEditor {...props} />
-)
+const CommentEditorContainer = props => {
+  return <TextEditor {...props} />;
+};
 
-const mapStateToProps = (state, ownProps) => (
-    {
-      parentPk: ownProps.commentParentPk || getPostDetailPk(state),
-      // should the editor be focues on after mounting?
-      focusOnEditor: ownProps.rootComment,
-    }
-)
+const mapStateToProps = (state, ownProps) => ({
+  parentPk: ownProps.commentParentPk || getPostDetailPk(state),
+  // should the editor be focues on after mounting?
+  focusOnEditor: ownProps.rootComment
+});
 
 const mapDispatchToProps = (dispatch, ownProps) => {
   // This is used for two occasions, creating and updating a comment.
   switch (ownProps.usage) {
-    case 'create':
-      const handleCreateCommentWrapper = (parentPk) => (commentBody) => dispatch(
-        makeCreateCommentRequest({
+    case "create": {
+      const handleSubmit = commentBody =>
+        dispatch(
+          makeCreateCommentRequest({
             body: commentBody,
-            parentPk,
-            rootComment: ownProps.rootComment,
+            parentPk: ownProps.postPk,
+            rootComment: ownProps.rootComment
           })
         );
-      
-      return {handleCreateCommentWrapper}
-    case 'update': {
-      const handleSubmit = (body) => {
+      return { handleSubmit };
+    }
+
+    case "update": {
+      const handleSubmit = body => {
         // onEditorSubmit is a hook so that the parent component
         // can effect changes beyond the action dispatch, e.g. toggle editor
-        ownProps.onEditorSubmit && ownProps.onEditorSubmit()
-        
+        ownProps.onEditorSubmit && ownProps.onEditorSubmit();
+
         return dispatch(
           makeUpdateCommentRequest({
             body,
-            pk: ownProps.pk,
+            pk: ownProps.pk
           })
         );
-      }
-      return ({ handleSubmit, })
+      };
+      return { handleSubmit };
     }
-    default:
-      return {}
-  }
-}
 
-const mergeProps = (stateProps, dispatchProps, ownProps) => {
-  // This mess is caused by the late determination of parentPk in mapStateToProps
-  if (ownProps.usage === 'create') {
-    const handleSubmit = dispatchProps.handleCreateCommentWrapper(
-      stateProps.parentPk
-    );
-    
-    return ({
-      ...stateProps,
-      ...ownProps,
-      handleSubmit,
-    });
+    default:
+      return {};
   }
-  return {...ownProps, ...stateProps, ...dispatchProps};
-}
+};
+
+// const mergeProps = (stateProps, dispatchProps, ownProps) => {
+//   // This mess is caused by the late determination of parentPk in mapStateToProps
+//   if (ownProps.usage === 'create') {
+//     const handleSubmit = dispatchProps.handleCreateCommentWrapper(
+//       stateProps.parentPk
+//     );
+//
+//     return ({
+//       ...stateProps,
+//       ...ownProps,
+//       handleSubmit,
+//     });
+//   }
+//   return {...ownProps, ...stateProps, ...dispatchProps};
+// }
 
 CommentEditorContainer.propTypes = {
-  usage: PropTypes.oneOf(['update', 'create']),
+  usage: PropTypes.oneOf(["update", "create"]).isRequired,
   rootComment: PropTypes.bool,
   commentParentPk: PropTypes.number,
   pk: PropTypes.number,
+  postPk: PropTypes.number,
   handleCreateComment: PropTypes.func,
   onEditorSubmit: PropTypes.func,
-}
+  handleSubmit: PropTypes.func.isRequired
+};
 
 export default connect(
   mapStateToProps,
-  mapDispatchToProps,
-  mergeProps,
+  mapDispatchToProps
 )(CommentEditorContainer);

b2e8837bd15b0a1511ceb62ad958cd8aebd3c0ef
Clint <cdunn6754@gmail.com>
2019-01-19 09:50:41 -0700

    Removes the build directory from gitignore
    For some reason building on the ec2 instance crashes the
    instance. I just built it on my dev mahcine an i'll use that.

---
+++ build/precache-manifest.9b11274ef932602fc8a7de216c97a830.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    revision: "a5cc7eb9292300741b53",
+    url: "/static/css/main.77e49f58.chunk.css"
+  },
+  {
+    revision: "a5cc7eb9292300741b53",
+    url: "/static/js/main.a5cc7eb9.chunk.js"
+  },
+  {
+    revision: "39193667cb332911bf8f",
+    url: "/static/css/1.3c093f04.chunk.css"
+  },
+  {
+    revision: "39193667cb332911bf8f",
+    url: "/static/js/1.39193667.chunk.js"
+  },
+  {
+    revision: "229c360febb4351a89df",
+    url: "/static/js/runtime~main.229c360f.js"
+  },
+  {
+    revision: "38cc4691e09d4412a90f529c9e04114d",
+    url: "/index.html"
+  }
+];

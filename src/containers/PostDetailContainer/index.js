84662df2754644acf47d48d401414b362f05240f
Clint <cdunn6754@gmail.com>
2019-01-20 15:53:39 -0700

    Fixes bug caused by refactor of subreddit component

--- src/containers/PostDetailContainer/index.js
+++ src/containers/PostDetailContainer/index.js
@@ -1,40 +1,39 @@
-import React, { Component } from 'react';
-import { connect } from 'react-redux';
+import React, { Component } from "react";
+import { connect } from "react-redux";
+import { withRouter } from "react-router";
 
-import PostDetail from '../../components/PostDetail';
+import PostDetail from "../../components/PostDetail";
 import {
   makePostDetailRequest,
   makeDeletePostRequest,
-  togglePostEditor,
-} from '../../actions/Posts';
+  togglePostEditor
+} from "../../actions/Posts";
 import {
   getPostDetailPk,
   getPostDetailBody,
   getPostDetailTitle,
   getPostDetailPosterUsername,
-  getPostDetailLoading,
-} from '../../reducers/postDetail';
-import { getAuthUsername, } from '../../reducers/userAuth';
+  getPostDetailLoading
+} from "../../reducers/postDetail";
+import { getAuthUsername } from "../../reducers/userAuth";
 import {
   getPostEditorShowState,
-  getEditPostError,
-} from '../../reducers/editPost';
-
+  getEditPostError
+} from "../../reducers/editPost";
 
 class PostDetailContainer extends Component {
   componentDidMount() {
     const postId = this.props.match.params.postId || null;
     this.props.fetchPostDetail(postId);
   }
-  
+
   componentWillUnmount() {
     if (this.props.showPostEditor) {
       this.props.togglePostEditor();
     }
   }
-  
+
   render() {
-        
     const {
       body,
       title,
@@ -47,51 +46,52 @@ class PostDetailContainer extends Component {
       editPostError,
       commentScroll,
       handleDeletePost,
-      togglePostEditor,
+      togglePostEditor
     } = this.props;
-    
-    return <PostDetail {...{
-        body,
-        title,
-        subredditTitle,
-        posterUsername,
-        authUsername,
-        pk,
-        loading,
-        showPostEditor,
-        editPostError,
-        commentScroll,
-        handleDeletePost,
-        togglePostEditor,
-      }} />;
+
+    return (
+      <PostDetail
+        {...{
+          body,
+          title,
+          subredditTitle,
+          posterUsername,
+          authUsername,
+          pk,
+          loading,
+          showPostEditor,
+          editPostError,
+          commentScroll,
+          handleDeletePost,
+          togglePostEditor
+        }}
+      />
+    );
   }
 }
 
-const mapStateToProps = (state, ownProps) => (
-  {
-    subredditTitle: ownProps.title,
-    body: getPostDetailBody(state),
-    title: getPostDetailTitle(state),
-    posterUsername: getPostDetailPosterUsername(state),
-    pk: getPostDetailPk(state),
-    loading: getPostDetailLoading(state),
-    showPostEditor: getPostEditorShowState(state),
-    editPostError: getEditPostError(state),
-    authUsername: getAuthUsername(state),
-  }
-)
+const mapStateToProps = (state, ownProps) => ({
+  subredditTitle: ownProps.title,
+  body: getPostDetailBody(state),
+  title: getPostDetailTitle(state),
+  posterUsername: getPostDetailPosterUsername(state),
+  pk: getPostDetailPk(state),
+  loading: getPostDetailLoading(state),
+  showPostEditor: getPostEditorShowState(state),
+  editPostError: getEditPostError(state),
+  authUsername: getAuthUsername(state)
+});
 
-const mapDispatchToProps = (dispatch, ownProps,) => (
-  {
-    fetchPostDetail: (postId) => dispatch(makePostDetailRequest(postId)),
-    handleDeletePost: () => dispatch(makeDeletePostRequest(
-        Number(ownProps.match.params.postId)
-      )),
-    togglePostEditor: () => dispatch(togglePostEditor()),
-  }
-)
+const mapDispatchToProps = (dispatch, ownProps) => ({
+  fetchPostDetail: postId => dispatch(makePostDetailRequest(postId)),
+  handleDeletePost: () =>
+    dispatch(makeDeletePostRequest(Number(ownProps.match.params.postId))),
+  togglePostEditor: () => dispatch(togglePostEditor())
+});
 
-export default connect(
-  mapStateToProps,
-  mapDispatchToProps,
-)(PostDetailContainer);
+export default withRouter(
+  connect(
+    mapStateToProps,
+    mapDispatchToProps
+  )(PostDetailContainer)
+);

492b869fed0201139d1600554b86899559c9d3f8
Clint <cdunn6754@gmail.com>
2019-01-21 16:13:33 -0700

    Redirects unauthed user from create post to login

--- src/components/CreatePost/index.js
+++ src/components/CreatePost/index.js
@@ -1,64 +1,68 @@
-import React , {Component} from 'react';
-import PropTypes from 'prop-types';
-import { withRouter } from 'react-router';
+import React, { Component } from "react";
+import PropTypes from "prop-types";
+import { withRouter } from "react-router";
 
-import TextEditor from '../TextEditor';
-import FieldGroup from '../FieldGroup';
-import { ErrorAlert } from '../AlertMessage';
-import { withMaybe } from '../../utilities/HOC';
-import FormButton from '../ModalForm/FormButton';
-import './styles.css';
+import TextEditor from "../TextEditor";
+import FieldGroup from "../FieldGroup";
+import { ErrorAlert } from "../AlertMessage";
+import { withMaybe } from "../../utilities/HOC";
+import FormButton from "../ModalForm/FormButton";
+import { SUBREDDIT_URL, HOME_SUBREDDIT_URL } from "../../urls";
+import "./styles.css";
 
 class CreatePost extends Component {
   constructor(props) {
     super(props);
-    
+
     this.state = {
-      title:'',
-    }
-    
+      title: ""
+    };
+
     this.handleSubmit = this.handleSubmit.bind(this);
   }
-  
-  handleTitleChange = (e) => this.setState({title: e.target.value});
-  
+
+  componentDidUpdate() {
+    // if you logout you can't be here
+    if (!this.props.authorizedUsername) {
+      this.props.showLoginModal();
+      this.props.history.push(HOME_SUBREDDIT_URL);
+    }
+  }
+
+  handleTitleChange = e => this.setState({ title: e.target.value });
+
   async handleSubmit(editorHtml) {
-    
     try {
       const postCreation = await this.props.handleCreatePost(
         this.state.title,
         editorHtml,
-        this.props.subredditTitle,
-      )
+        this.props.subredditTitle
+      );
       this.props.history.replace(`/r/${this.props.subredditTitle}`);
     } catch {
-      this.forceUpdate()
+      this.forceUpdate();
     }
   }
-  
+
   render() {
-    
-    const {
-      errorMessage,
-      loading,
-    } = this.props;
-      
-    const CreatePostErrorAlert = withMaybe((props) => props.children)(ErrorAlert);
-      
+    const { errorMessage, loading } = this.props;
+
+    const CreatePostErrorAlert = withMaybe(props => props.children)(ErrorAlert);
+
     return (
       <div className="create-post-container">
         <div className="alert-message-container">
-          <CreatePostErrorAlert children={this.props.errorMessage}/>
+          <CreatePostErrorAlert children={errorMessage} />
         </div>
         <form>
-          <div className='title-input-container'>
+          <div className="title-input-container">
             <FieldGroup
               id="create-post-title"
-              placeholder='Title'
-              type='text'
+              placeholder="Title"
+              type="text"
               value={this.state.title}
               onChange={this.handleTitleChange}
-              name='username'
+              name="username"
             />
           </div>
 
@@ -67,18 +71,19 @@ class CreatePost extends Component {
             usage="create"
             placeholder="Text (optional)"
           />
-
         </form>
       </div>
-  )
+    );
   }
 }
 
 CreatePost.propTypes = {
+  authorizedUsername: PropTypes.string,
   errorMessage: PropTypes.string,
   loading: PropTypes.bool,
   subredditTitle: PropTypes.string,
   handleCreatePost: PropTypes.func,
+  showLoginModal: PropTypes.func
 };
 
 export default withRouter(CreatePost);
--- src/containers/CreatePostContainer/index.js
+++ src/containers/CreatePostContainer/index.js
@@ -1,33 +1,34 @@
-import React, { Component } from 'react';
-import { connect } from 'react-redux';
+import React, { Component } from "react";
+import { connect } from "react-redux";
 
-import CreatePost from '../../components/CreatePost';
-import { makeCreatePostRequest } from '../../actions/Posts';
+import CreatePost from "../../components/CreatePost";
+import { makeCreatePostRequest } from "../../actions/Posts";
+import { showUserAuthModal } from "../../actions/UserAuthModal";
 import {
   getCreatePostError,
-  getCreatePostLoading,
-} from '../../reducers/createPost';
-import {
-  getSubredditTitle,
-} from '../../reducers/subreddit';
+  getCreatePostLoading
+} from "../../reducers/createPost";
+import { getSubredditTitle } from "../../reducers/subreddit";
+import { getAuthUsername } from "../../reducers/userAuth";
 
 class CreatePostContainer extends Component {
-
   render() {
-    return <CreatePost {...this.props} />
+    return <CreatePost {...this.props} />;
   }
 }
 
-const mapDispatchToProps = (dispatch) => ({
-    handleCreatePost: (title, body, subredditTitle) =>
-      dispatch(makeCreatePostRequest(title, body, subredditTitle)),
-})
+const mapStateToProps = state => ({
+  errorMessage: getCreatePostError(state),
+  loading: getCreatePostLoading(state),
+  subredditTitle: getSubredditTitle(state),
+  authorizedUsername: getAuthUsername(state)
+});
 
-const mapStateToProps = (state) => ({
-    errorMessage: getCreatePostError(state),
-    loading: getCreatePostLoading(state),
-    subredditTitle: getSubredditTitle(state),
-})
+const mapDispatchToProps = dispatch => ({
+  handleCreatePost: (title, body, subredditTitle) =>
+    dispatch(makeCreatePostRequest(title, body, subredditTitle)),
+  showLoginModal: () => dispatch(showUserAuthModal("login"))
+});
 
 export default connect(
   mapStateToProps,

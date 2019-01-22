ce18306b1c014c329aaf5316dd15530de4520b34
Clint <cdunn6754@gmail.com>
2019-01-21 17:51:24 -0700

    Shows an unauthed voter thelogin modal

--- src/actions/Voter/index.js
+++ src/actions/Voter/index.js
@@ -4,45 +4,48 @@ import {
   VOTE_FAILURE,
   COMMENT_VOTE_SUCCESS,
   POST_VOTE_SUCCESS,
-  API_VOTE,
-} from '../actionTypes';
+  API_VOTE
+} from "../actionTypes";
 
-import { voteApi } from '../../api/Voter';
+import { voteApi } from "../../api/Voter";
+import { getAuthUsername } from "../../reducers/userAuth";
+import { showUserAuthModal } from "../UserAuthModal";
 
 // use thunk to get token
-export const makeVoteRequest = (voteData) =>
-  (dispatch, getState) =>
-    dispatch(
-      {
-        type: API_VOTE,
-        types: {
-          request: VOTE_REQUEST,
-          success: onVoteSuccess,
-          failure: VOTE_FAILURE,
-        },
-        callAPI: () => voteApi(
-          voteData,
-          getState().userAuth.token
-        ),
-      }
-    )
+export const makeVoteRequest = voteData => (dispatch, getState) => {
+  const state = getState();
+
+  if (!getAuthUsername(state)) {
+    return dispatch(showUserAuthModal("login"));
+  }
+
+  return dispatch({
+    type: API_VOTE,
+    types: {
+      request: VOTE_REQUEST,
+      success: onVoteSuccess,
+      failure: VOTE_FAILURE
+    },
+    callAPI: () => voteApi(voteData, getState().userAuth.token)
+  });
+};
 
 const onVoteSuccess = (data, getState, dispatch) => {
-  if (data.hasOwnProperty('comment')) {
+  if (data.hasOwnProperty("comment")) {
     return dispatch({
       type: COMMENT_VOTE_SUCCESS,
       data
-    })
-  } else if (data.hasOwnProperty('post')) {
+    });
+  } else if (data.hasOwnProperty("post")) {
     return dispatch({
       type: POST_VOTE_SUCCESS,
       data
-    })
+    });
   }
-  
+
   throw new ReferenceError(
-    'Upvote is of unknown type',
-    'actions/Voter/index.js',
+    "Upvote is of unknown type",
+    "actions/Voter/index.js",
     41
-  )
-}
+  );
+};

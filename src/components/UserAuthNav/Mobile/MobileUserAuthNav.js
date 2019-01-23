d84e18dfa2a3f28244275791372e714576a91124
Clint <cdunn6754@gmail.com>
2019-01-23 12:59:34 -0700

    Fixes bug with non-closing mobile dropdown menu

--- src/components/UserAuthNav/Mobile/MobileUserAuthNav.js
+++ src/components/UserAuthNav/Mobile/MobileUserAuthNav.js
@@ -1,4 +1,4 @@
-import React, { Fragment } from "react";
+import React, { Component, Fragment } from "react";
 import PropTypes from "prop-types";
 import { withRouter } from "react-router";
 import { MenuItem, Dropdown } from "react-bootstrap";
@@ -14,72 +14,89 @@ import { FaUserPlus } from "react-icons/fa";
 
 import "./styles.css";
 
-const MobileUserAuthNav = props => {
-  const {
-    authUsername,
-    handleLogout,
-    showModal,
-    redirectToCreatePost,
-    redirectToCreateSubreddit
-  } = props;
+// This is a class because the dropdown doesn't hide well on mobile for
+// some reason so state is needed to fix it
+class MobileUserAuthNav extends Component {
+  constructor(props) {
+    super(props);
 
-  const authenticatedMenu = (
-    <Fragment>
-      <MenuItem eventKey={3.1} onClick={redirectToCreatePost}>
-        <IoMdSettings /> Edit profile
-      </MenuItem>
-      <MenuItem eventKey={3.1} onClick={redirectToCreatePost}>
-        <IoMdCreate /> Create post
-      </MenuItem>
-      <MenuItem eventKey={3.2} onClick={redirectToCreateSubreddit}>
-        <IoMdCreate /> Create subreddit
-      </MenuItem>
-      <MenuItem divider />
-      <MenuItem eventKey={3.4} onClick={handleLogout}>
-        <IoMdLogOut /> Logout
-      </MenuItem>
-      <MenuItem
-        eventKey={3.5}
-        onClick={() => handleLogout()}
-        href="https://github.com/cdunn6754/reReddit_frontend"
-        target="_blank"
-      >
-        <IoLogoGithub /> reReddit GitHub
-      </MenuItem>
-    </Fragment>
-  );
+    this.state = { showMenu: false };
+  }
 
-  const unAuthenticatedMenu = (
-    <Fragment>
-      <MenuItem eventKey="1" onClick={() => showModal("login")}>
-        <IoMdLogIn /> Sign in
-      </MenuItem>
-      <MenuItem eventKey="1" onClick={() => showModal("register")}>
-        <FaUserPlus /> Sign up
-      </MenuItem>
-      <MenuItem divider />
-      <MenuItem
-        eventKey="2"
-        href="https://github.com/cdunn6754/reReddit_frontend"
-        target="_blank"
-      >
-        <IoLogoGithub /> reReddit Github
-      </MenuItem>
-    </Fragment>
-  );
+  toggleShowMenu = () => this.setState({ showMenu: !this.state.showMenu });
 
-  return (
-    <div id="mobile-user-auth-dropdown-container">
-      <Dropdown pullRight id="mobile-user-dropdown">
-        <Dropdown.Toggle>
-          <IoMdMenu />
-        </Dropdown.Toggle>
-        <Dropdown.Menu>
-          {authUsername ? authenticatedMenu : unAuthenticatedMenu}
-        </Dropdown.Menu>
-      </Dropdown>
-    </div>
-  );
-};
+  render() {
+    const {
+      authUsername,
+      handleLogout,
+      showModal,
+      redirectToCreatePost,
+      redirectToCreateSubreddit
+    } = this.props;
+
+    const authenticatedMenu = (
+      <Fragment>
+        <MenuItem eventKey={3.1} onClick={redirectToCreatePost}>
+          <IoMdSettings /> Edit profile
+        </MenuItem>
+        <MenuItem eventKey={3.1} onClick={redirectToCreatePost}>
+          <IoMdCreate /> Create post
+        </MenuItem>
+        <MenuItem eventKey={3.2} onClick={redirectToCreateSubreddit}>
+          <IoMdCreate /> Create subreddit
+        </MenuItem>
+        <MenuItem divider />
+        <MenuItem eventKey={3.4} onClick={handleLogout}>
+          <IoMdLogOut /> Logout
+        </MenuItem>
+        <MenuItem
+          eventKey={3.5}
+          onClick={() => handleLogout()}
+          href="https://github.com/cdunn6754/reReddit_frontend"
+          target="_blank"
+        >
+          <IoLogoGithub /> reReddit GitHub
+        </MenuItem>
+      </Fragment>
+    );
+
+    const unAuthenticatedMenu = (
+      <Fragment>
+        <MenuItem eventKey="1" onClick={() => showModal("login")}>
+          <IoMdLogIn /> Sign in
+        </MenuItem>
+        <MenuItem eventKey="1" onClick={() => showModal("register")}>
+          <FaUserPlus /> Sign up
+        </MenuItem>
+        <MenuItem divider />
+        <MenuItem
+          eventKey="2"
+          href="https://github.com/cdunn6754/reReddit_frontend"
+          target="_blank"
+        >
+          <IoLogoGithub /> reReddit Github
+        </MenuItem>
+      </Fragment>
+    );
+
+    return (
+      <div id="mobile-user-auth-dropdown-container">
+        <Dropdown
+          pullRight
+          id="mobile-user-dropdown"
+          onToggle={this.toggleShowMenu}
+          open={this.state.showMenu}
+        >
+          <Dropdown.Toggle>
+            <IoMdMenu />
+          </Dropdown.Toggle>
+          <Dropdown.Menu onClick={this.toggleShowMenu}>
+            {authUsername ? authenticatedMenu : unAuthenticatedMenu}
+          </Dropdown.Menu>
+        </Dropdown>
+      </div>
+    );
+  }
+}
 
 export default withRouter(MobileUserAuthNav);

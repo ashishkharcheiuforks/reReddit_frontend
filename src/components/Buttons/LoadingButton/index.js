492b824caea6ae7767017ffd585980846d7edacf
Clint <cdunn6754@gmail.com>
2019-01-24 17:05:07 -0700

    Improves LoadingButton to retain size when loading

--- src/components/Buttons/LoadingButton/index.js
+++ src/components/Buttons/LoadingButton/index.js
@@ -1,32 +1,31 @@
-import React from 'react';
-import { Button } from 'react-bootstrap';
-import { CircleLoader } from 'react-spinners';
+import React from "react";
+import { Button } from "react-bootstrap";
+import { CircleLoader } from "react-spinners";
 
-import './styles.css'
+import "./styles.css";
 
-const LoadingButton = (props) => {
-  
-  const {
-    loading,
-    children,
-    className,
-    ...rest
-  } = props;
-  
+const LoadingButton = props => {
+  const { loading, children, className, ...rest } = props;
+
   const spinner = <CircleLoader size={20} className="display: inline-block" />;
-  
+
+  // when loading make the text transparent, this retains the button size
+  const style = loading
+    ? {
+        color: "transparent"
+      }
+    : {};
+
   return (
     <div className="loading-button-container">
-      <Button
-        {...rest}
-        className={ className || "loading-button"}
-        disabled={loading}
-        
-      >
-        {loading ? spinner : children}
+      <Button {...rest} className={className || "loading-button"} style={style}>
+        <div className="button-spinner-container">
+          {loading ? spinner : null}
+        </div>
+        {children}
       </Button>
     </div>
-  )
-}
+  );
+};
 
 export default LoadingButton;

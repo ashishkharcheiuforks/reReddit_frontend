742a4bbab4f1f21c9035597192d32d4db139d4cf
Clint <cdunn6754@gmail.com>
2019-01-30 15:00:56 -0700

    Adds a picture to the create subreddit view

--- src/components/CreateSubreddit/index.js
+++ src/components/CreateSubreddit/index.js
@@ -1,69 +1,69 @@
-import React, { Component } from 'react';
-import PropTypes from 'prop-types';
-import { withRouter } from 'react-router';
+import React, { Component } from "react";
+import PropTypes from "prop-types";
+import { withRouter } from "react-router";
 
-import TextEditor from '../TextEditor';
-import FieldGroup from  '../FieldGroup';
-import { ErrorAlertWithError } from '../AlertMessage';
-import FormButton from '../ModalForm/FormButton';
-import './styles.css';
+import TextEditor from "../TextEditor";
+import FieldGroup from "../FieldGroup";
+import { ErrorAlertWithError } from "../AlertMessage";
+import FormButton from "../ModalForm/FormButton";
+import "./styles.css";
 
 class CreateSubreddit extends Component {
   constructor(props) {
     super(props);
     this.state = {
-      description: '',
-      title: '',
+      description: "",
+      title: ""
     };
-    
+
     this.handleSubmit = this.handleSubmit.bind(this);
   }
-  
-  handleTitleChange = (e) => this.setState({title: e.target.value});
-  
-  handleDescriptionChange = (e) =>
-    this.setState({description: e.target.value});
-    
+
+  handleTitleChange = e => this.setState({ title: e.target.value });
+
+  handleDescriptionChange = e => this.setState({ description: e.target.value });
+
   async handleSubmit() {
     try {
       await this.props.handleCreateSubreddit({
         title: this.state.title,
-        description: this.state.description,
+        description: this.state.description
       });
       this.props.history.push(`/r/${this.state.title}`);
-    }
-    catch {
+    } catch {
       this.forceUpdate();
     }
   }
-    
+
   render() {
-    const {
-      errorMessage,
-      loading,
-      handleCreateSubreddit,
-    } = this.props;
-    
+    const { errorMessage, loading, handleCreateSubreddit } = this.props;
+
     return (
       <div className="create-subreddit-container">
+        <div id="picture-container">
+          <a
+            href={`https://www.pexels.com/photo/assorted-colored-wooden-planks-985287/`}
+            target="_blank"
+          >
+            Photo by Magda Ehlers from Pexels
+          </a>
+        </div>
         <div className="content-container">
           <div className="form-container">
-            
             <h2>Create a Subreddit</h2>
-            <hr/>
-            <ErrorAlertWithError>
-              {errorMessage}
-            </ErrorAlertWithError>
-            
+            <hr />
+            <ErrorAlertWithError>{errorMessage}</ErrorAlertWithError>
+
             <form>
               <div className="input-container">
                 <FieldGroup
                   id="title-input"
                   placeholder="title"
-                  type='text'
+                  type="text"
                   value={this.state.title}
                   onChange={this.handleTitleChange}
-                  name='title'
+                  name="title"
+                  autoComplete="off"
                 />
                 <FieldGroup
                   id="description-input"
@@ -84,16 +84,15 @@ class CreateSubreddit extends Component {
             </form>
           </div>
         </div>
-          
       </div>
-    )
+    );
   }
 }
 
 CreateSubreddit.propTypes = {
   loading: PropTypes.bool,
   error: PropTypes.string,
-  handleCreateSubreddit: PropTypes.func,
-}
+  handleCreateSubreddit: PropTypes.func
+};
 
 export default withRouter(CreateSubreddit);

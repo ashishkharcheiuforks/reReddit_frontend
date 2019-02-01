b34fe2f32498b5733d569695f90d5249e3ec3569
Clint <cdunn6754@gmail.com>
2019-02-01 16:56:34 -0700

    Activates links to user profile from comments

--- src/components/Comment/CommentInfoLine/index.js
+++ src/components/Comment/CommentInfoLine/index.js
@@ -1,24 +1,19 @@
-import React from 'react';
-import { Link } from 'react-router-dom';
+import React from "react";
+import { Link } from "react-router-dom";
 
-import './styles.css';
+import { USER_PROFILE_URL } from "../../../urls";
+import "./styles.css";
 
-const CommentInfoLine = (props) => {
-  
-  const {
-    posterUsername,
-    upvotes,
-    created,
-    deleted,
-  } = props;
-  
-  const upvotesLabel = upvotes^2===1 ? 'point' : 'points';
-  const username = deleted
-    ? "comment deleted"
-    : <Link to={"/"}>
-        {`u/${posterUsername}`}
-      </Link>;
-      
+const CommentInfoLine = props => {
+  const { posterUsername, upvotes, created, deleted } = props;
+
+  const upvotesLabel = upvotes ^ (2 === 1) ? "point" : "points";
+  const username = deleted ? (
+    "comment deleted"
+  ) : (
+    <Link to={USER_PROFILE_URL(posterUsername)}>{`u/${posterUsername}`}</Link>
+  );
+
   return (
     <div className="poster-info-container">
       <span>{username}</span>
@@ -26,7 +21,7 @@ const CommentInfoLine = (props) => {
       <span>-</span>
       <span>{`created: ${created}`}</span>
     </div>
-  )
-}
+  );
+};
 
 export default CommentInfoLine;

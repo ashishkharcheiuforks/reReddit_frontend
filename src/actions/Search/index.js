516ddd9e883f225cb5c73d5b9d1562adfc3608ca
Clint <cdunn6754@gmail.com>
2019-02-03 07:50:58 -0700

    Search results now display previous post votes

--- src/actions/Search/index.js
+++ src/actions/Search/index.js
@@ -8,18 +8,20 @@ import {
 } from "../actionTypes";
 
 import { searchApi } from "../../api/Search";
+import { getAuthUserToken } from "../../reducers/userAuth";
 
-export const makeSearchRequest = q => {
-  return {
+// If authenticated we can access the previous votes to make
+// the arrow change colors
+export const makeSearchRequest = q => (dispatch, getState) =>
+  dispatch({
     type: API_SEARCH,
     types: {
       request: SEARCH_REQUEST,
       success: SEARCH_SUCCESS,
       failure: SEARCH_FAILURE
     },
-    callAPI: () => searchApi(q)
-  };
-};
+    callAPI: () => searchApi(q, getAuthUserToken(getState()))
+  });
 
 export const setSearchQuery = q => ({
   type: SET_SEARCH_QUERY,
--- src/api/Search/index.js
+++ src/api/Search/index.js
@@ -1,8 +1,9 @@
-import axios from 'axios';
+import axios from "axios";
 
-import { SEARCH_URL } from '../constants'
+import { SEARCH_URL } from "../constants";
+import { tokenContextObj } from "../apiUtils";
 
-export const searchApi = (q) => (
-  axios.get(SEARCH_URL, {params: { q, }, })
-    .then(response => response.data)
-)
+export const searchApi = (q, token) =>
+  axios
+    .get(SEARCH_URL, { params: { q }, ...tokenContextObj(token) })
+    .then(response => response.data);

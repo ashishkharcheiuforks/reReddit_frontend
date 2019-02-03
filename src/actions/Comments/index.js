b581b433842c0d4126d8d83d1ee06e3830357b86
Clint <cdunn6754@gmail.com>
2019-02-03 07:34:56 -0700

    Removes username GET query param from comment tree list request
    Just as with the posts, this functionality now relies on an
    authenticated user, the query param method has be deprecated
    in the backend.

--- src/actions/Comments/index.js
+++ src/actions/Comments/index.js
@@ -1,4 +1,4 @@
-import { normalize, schema } from 'normalizr';
+import { normalize, schema } from "normalizr";
 
 import {
   FETCH_POST_COMMENT_TREES_REQUEST,
@@ -17,110 +17,93 @@ import {
   UPDATE_COMMENT_SUCCESS,
   UPDATE_COMMENT_FAILURE,
   API_UPDATE_COMMENT,
-  COMMENT_VOTE_SUCCESS,
-} from '../actionTypes';
+  COMMENT_VOTE_SUCCESS
+} from "../actionTypes";
 import {
   createCommentApi,
   getCommentTreeApi,
   commentVoteApi,
   deleteCommentApi,
-  updateCommentApi,
-} from '../../api/Comments'
+  updateCommentApi
+} from "../../api/Comments";
+import { getAuthUserToken } from "../../reducers/userAuth";
 
-// Use redux-thunk to get the userAuth username
-export const makeCommentTreeRequest = (postPk) =>
-  (dispatch, getState) => dispatch(
-    {
-      type: API_POST_COMMENT_TREES,
-      types: {
-        request: FETCH_POST_COMMENT_TREES_REQUEST,
-        success: normalizeCommentTreeOnSuccess,
-        failure: FETCH_POST_COMMENT_TREES_FAILURE,
-      },
-      callAPI: () => getCommentTreeApi(postPk, getState().userAuth.username),
-    }
-  )
-  
+// Use redux-thunk to get the userAuth token
+export const makeCommentTreeRequest = postPk => (dispatch, getState) =>
+  dispatch({
+    type: API_POST_COMMENT_TREES,
+    types: {
+      request: FETCH_POST_COMMENT_TREES_REQUEST,
+      success: normalizeCommentTreeOnSuccess,
+      failure: FETCH_POST_COMMENT_TREES_FAILURE
+    },
+    callAPI: () => getCommentTreeApi(postPk, getAuthUserToken(getState()))
+  });
+
 // When comment trees are successfully fetched, normalize them for redux store
 const normalizeCommentTreeOnSuccess = (nestedComments, getState, dispatch) => {
   if (nestedComments.length === 0) {
     return dispatch({
       type: FETCH_POST_COMMENT_TREES_SUCCESS,
-      data: {entities: {comments: {}, posters: {} }, result: [] },
-    })
+      data: { entities: { comments: {}, posters: {} }, result: [] }
+    });
   }
-  const posterSchema = new schema.Entity('posters', {}, { idAttribute: 'pk' });
+  const posterSchema = new schema.Entity("posters", {}, { idAttribute: "pk" });
   const commentSchema = new schema.Entity(
-    'comments',
-    {poster: posterSchema},
-    { idAttribute: 'pk',}
+    "comments",
+    { poster: posterSchema },
+    { idAttribute: "pk" }
   );
   const commentListSchema = new schema.Array(commentSchema);
-  commentSchema.define({ children: commentListSchema })
+  commentSchema.define({ children: commentListSchema });
   const normalizedComments = normalize(nestedComments, commentListSchema);
-  Object.entries(normalizedComments.entities.comments).forEach(([id, comment]) => {
-    const {vote_state, ...newComment} = comment;
-    normalizedComments.entities.comments[id] = {
-      ...newComment,
-      voteDisplayState: vote_state
-    };
-  });
+  Object.entries(normalizedComments.entities.comments).forEach(
+    ([id, comment]) => {
+      const { vote_state, ...newComment } = comment;
+      normalizedComments.entities.comments[id] = {
+        ...newComment,
+        voteDisplayState: vote_state
+      };
+    }
+  );
   return dispatch({
     type: FETCH_POST_COMMENT_TREES_SUCCESS,
     data: normalizedComments
-  })
-}
+  });
+};
 
 // Use redux-thunk to grab the userAuth token
-export const makeCreateCommentRequest = (commentData) =>
-  (dispatch, getState) =>
-    dispatch(
-      {
-        type: API_CREATE_COMMENT,
-        types: {
-          request: CREATE_COMMENT_REQUEST,
-          success: CREATE_COMMENT_SUCCESS,
-          failure: CREATE_COMMENT_FAILURE,
-        },
-        callAPI: () => createCommentApi(
-          commentData,
-          getState().userAuth.token
-        ),
-      }
-    )
-    
+export const makeCreateCommentRequest = commentData => (dispatch, getState) =>
+  dispatch({
+    type: API_CREATE_COMMENT,
+    types: {
+      request: CREATE_COMMENT_REQUEST,
+      success: CREATE_COMMENT_SUCCESS,
+      failure: CREATE_COMMENT_FAILURE
+    },
+    callAPI: () => createCommentApi(commentData, getState().userAuth.token)
+  });
+
 // Use redux-thunk to grab the userAuth token
-export const makeDeleteCommentRequest = (pk) =>
-  (dispatch, getState) =>
-    dispatch(
-      {
-        type: API_DELETE_COMMENT,
-        types: {
-          request: DELETE_COMMENT_REQUEST,
-          success: DELETE_COMMENT_SUCCESS,
-          failure: DELETE_COMMENT_FAILURE,
-        },
-        callAPI: () => deleteCommentApi(
-          pk,
-          getState().userAuth.token
-        ),
-      }
-    )
-    
+export const makeDeleteCommentRequest = pk => (dispatch, getState) =>
+  dispatch({
+    type: API_DELETE_COMMENT,
+    types: {
+      request: DELETE_COMMENT_REQUEST,
+      success: DELETE_COMMENT_SUCCESS,
+      failure: DELETE_COMMENT_FAILURE
+    },
+    callAPI: () => deleteCommentApi(pk, getState().userAuth.token)
+  });
+
 // Use redux-thunk to grab the userAuth token
-export const makeUpdateCommentRequest = (commentData) =>
-  (dispatch, getState) =>
-    dispatch(
-      {
-        type: API_UPDATE_COMMENT,
-        types: {
-          request: UPDATE_COMMENT_REQUEST,
-          success: UPDATE_COMMENT_SUCCESS,
-          failure: UPDATE_COMMENT_FAILURE,
-        },
-        callAPI: () => updateCommentApi(
-          commentData,
-          getState().userAuth.token
-        ),
-      }
-    )
+export const makeUpdateCommentRequest = commentData => (dispatch, getState) =>
+  dispatch({
+    type: API_UPDATE_COMMENT,
+    types: {
+      request: UPDATE_COMMENT_REQUEST,
+      success: UPDATE_COMMENT_SUCCESS,
+      failure: UPDATE_COMMENT_FAILURE
+    },
+    callAPI: () => updateCommentApi(commentData, getState().userAuth.token)
+  });

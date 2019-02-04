ff37e3cf86c23935c1c695acb6899cf0890ec8e2
Clint <cdunn6754@gmail.com>
2019-02-04 16:32:28 -0700

    Smart label on comment points

--- src/components/Comment/CommentInfoLine/index.js
+++ src/components/Comment/CommentInfoLine/index.js
@@ -7,7 +7,7 @@ import "./styles.css";
 const CommentInfoLine = props => {
   const { posterUsername, upvotes, created, deleted } = props;
 
-  const upvotesLabel = upvotes ^ (2 === 1) ? "point" : "points";
+  const upvotesLabel = upvotes === 1 ? "point" : "points";
   const username = deleted ? (
     "comment deleted"
   ) : (

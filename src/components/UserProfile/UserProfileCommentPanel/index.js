cbeb622f9acd636ca54057a7c09a5ef064dbca4c
Clint <cdunn6754@gmail.com>
2019-03-15 10:20:00 -0600

    Styles the comment body section of the userprofile comment view

--- src/components/UserProfile/UserProfileCommentPanel/index.js
+++ src/components/UserProfile/UserProfileCommentPanel/index.js
@@ -15,7 +15,18 @@ const UserProfileCommentPanel = props => {
 
   const comments = commentList.map(comment => (
     <div className="up-comment-panel" key={comment.pk}>
-      {comment.body}
+      <div className="up-comment-title-line">
+        <Link className="up-ctl-username" to={USER_PROFILE_URL(username)}>
+          {username}
+        </Link>
+        <span className="up-ctl-points">points: {comment.upvotes}</span>
+        <span className="up-dot">{"\u00b7"}</span>
+        <span className="up-ctl-created">{comment.created}</span>
+      </div>
+      <div
+        className="up-comment-body"
+        dangerouslySetInnerHTML={{ __html: comment.body }}
+      />
     </div>
   ));
 
@@ -23,26 +34,28 @@ const UserProfileCommentPanel = props => {
     <div className="up-comment-post-panel">
       <div className="up-post-panel">
         <FaRegCommentAlt className="up-comment-icon" />
-        <span className="up-postline-username">{username}</span>
-        <span className="up-commented-on">commented on</span>
-        {postInfo.title}
-        <span className="up-postline-dot">{"\u00b7"}</span>
-        <Link
-          className="up-postline-subreddit-link"
-          to={SUBREDDIT_URL(postInfo.subreddit_title)}
-        >
-          r/{postInfo.subreddit_title}
-        </Link>
-        <span className="up-postline-dot">{"\u00b7"}</span>
-        <span className="up-postline-posted-by">
-          Posted by:
+        <div className="up-post-panel-text">
+          <span className="up-postline-username">{username}</span>
+          <span className="up-commented-on">commented on</span>
+          {postInfo.title}
+          <span className="up-dot">{"\u00b7"}</span>
           <Link
-            className="up-postline-username-link"
-            to={USER_PROFILE_URL(postInfo.poster_username)}
+            className="up-postline-subreddit-link"
+            to={SUBREDDIT_URL(postInfo.subreddit_title)}
           >
-            u/{postInfo.poster_username}
+            r/{postInfo.subreddit_title}
           </Link>
-        </span>
+          <span className="up-dot">{"\u00b7"}</span>
+          <span className="up-postline-posted-by">
+            Posted by:
+            <Link
+              className="up-postline-username-link"
+              to={USER_PROFILE_URL(postInfo.poster_username)}
+            >
+              u/{postInfo.poster_username}
+            </Link>
+          </span>
+        </div>
       </div>
       {comments}
     </div>

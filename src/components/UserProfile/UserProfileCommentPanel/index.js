0a3de5e761f393f58e0ddca8b5d57515e62bd142
Clint <cdunn6754@gmail.com>
2019-03-15 09:51:10 -0600

    Styles the post line of the comments in user profile

--- src/components/UserProfile/UserProfileCommentPanel/index.js
+++ src/components/UserProfile/UserProfileCommentPanel/index.js
@@ -1,19 +1,82 @@
 import React from "react";
 import PropTypes from "prop-types";
+import { FaRegCommentAlt } from "react-icons/fa";
+import { Link } from "react-router-dom";
+
+import {
+  POST_DETAIL_URL,
+  USER_PROFILE_URL,
+  SUBREDDIT_URL
+} from "../../../urls";
+import "./styles.css";
 
 const UserProfileCommentPanel = props => {
-  const { commentList, postInfo } = props;
+  const { commentList, postInfo, username } = props;
 
   const comments = commentList.map(comment => (
-    <div className="up-comment-panel">{comment.body}</div>
+    <div className="up-comment-panel" key={comment.pk}>
+      {comment.body}
+    </div>
   ));
 
   return (
     <div className="up-comment-post-panel">
-      <div className="up-post-panel">{postInfo.title}</div>
+      <div className="up-post-panel">
+        <FaRegCommentAlt className="up-comment-icon" />
+        <span className="up-postline-username">{username}</span>
+        <span className="up-commented-on">commented on</span>
+        {postInfo.title}
+        <span className="up-postline-dot">{"\u00b7"}</span>
+        <Link
+          className="up-postline-subreddit-link"
+          to={SUBREDDIT_URL(postInfo.subreddit_title)}
+        >
+          r/{postInfo.subreddit_title}
+        </Link>
+        <span className="up-postline-dot">{"\u00b7"}</span>
+        <span className="up-postline-posted-by">
+          Posted by:
+          <Link
+            className="up-postline-username-link"
+            to={USER_PROFILE_URL(postInfo.poster_username)}
+          >
+            u/{postInfo.poster_username}
+          </Link>
+        </span>
+      </div>
       {comments}
     </div>
   );
 };
 
+UserProfileCommentPanel.propTypes = {
+  commentList: PropTypes.arrayOf(
+    PropTypes.shape({
+      post: PropTypes.number,
+      poster: PropTypes.number,
+      parent: PropTypes.number,
+      body: PropTypes.string,
+      upvotes: PropTypes.number,
+      pk: PropTypes.number,
+      vote_state: PropTypes.number,
+      deleted: PropTypes.bool,
+      created: PropTypes.string
+    })
+  ),
+  postInfo: PropTypes.shape({
+    poster: PropTypes.number,
+    created: PropTypes.string,
+    updated: PropTypes.string,
+    parent: PropTypes.number,
+    body: PropTypes.string,
+    title: PropTypes.string,
+    upvotes: PropTypes.number,
+    subreddit: PropTypes.number,
+    subreddit_title: PropTypes.string,
+    poster_username: PropTypes.string,
+    pk: PropTypes.number,
+    vote_state: PropTypes.number
+  }),
+  username: PropTypes.string
+};
 export default UserProfileCommentPanel;
--- src/containers/UserProfileCommentPanelContainer/index.js
+++ src/containers/UserProfileCommentPanelContainer/index.js
@@ -4,17 +4,19 @@ import { connect } from "react-redux";
 import UserProfileCommentPanel from "../../components/UserProfile/UserProfileCommentPanel";
 import {
   getUserProfileCommentsByPostId,
-  getUserProfileCommentPostById
+  getUserProfileCommentPostById,
+  getUserProfileUsername
 } from "../../reducers/userProfile";
 
 const UserProfileCommentPanelContainer = props => {
-  const { commentList, postInfo } = props;
-  return <UserProfileCommentPanel {...{ commentList, postInfo }} />;
+  const { commentList, postInfo, username } = props;
+  return <UserProfileCommentPanel {...{ commentList, postInfo, username }} />;
 };
 
 const mapStateToProps = (state, ownProps) => ({
   commentList: getUserProfileCommentsByPostId(state, ownProps.postPk),
-  postInfo: getUserProfileCommentPostById(state, ownProps.postPk)
+  postInfo: getUserProfileCommentPostById(state, ownProps.postPk),
+  username: getUserProfileUsername(state)
 });
 
 export default connect(mapStateToProps)(UserProfileCommentPanelContainer);
